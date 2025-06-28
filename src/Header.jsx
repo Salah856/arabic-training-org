c977894b626f7d7b37dcdb98da3bad00e7f226ae
Salah <salah_elhossiny856@yahoo.com>
2025-06-28 23:13:04 +0300

    init app

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,13 @@ const Header = () => {
       <div className="container">
         <nav className="navbar">
           <div className="logo-container">
-            <img src={logo} alt="المؤسسة العربية للتدريب والتنمية المستدامة" className="logo" />
+            <img 
+                src={logo} 
+                alt="المؤسسة العربية للتدريب والتنمية المستدامة" 
+                className="logo" 
+                width="400"
+                height="400"
+            />
           </div>
           <h1 className="title">المؤسسة العربية للتدريب والتنمية المستدامة</h1>
           <div className="commercial-number">سجل تجاري رقم: 14446</div>
--- src/Programs.jsx
+++ src/Programs.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import Pic_1 from "./assets/pic_1.jpeg"; 
+import Pic_2 from "./assets/pic_2.jpeg";
+import Pic_3 from "./assets/pic_3.jpeg";
+import Pic_4 from "./assets/pic_4.jpeg";
+
 
 const Programs = () => {
   const trainingPrograms = [
@@ -20,6 +25,49 @@ const Programs = () => {
               <h3>{program}</h3>
             </div>
           ))}
+        </div>
+
+        <div className="programs-images">
+                <div className="image-container">
+                    <img 
+                    src={Pic_1} 
+                    alt="برنامج تدريبي 1" 
+                    className="program-image"
+                    onMouseEnter={(e) => e.currentTarget.classList.add('hover-effect')}
+                    onMouseLeave={(e) => e.currentTarget.classList.remove('hover-effect')}
+                    />
+                    {/* <div className="image-caption">ممارس البرمجة اللغوية العصبية</div> */}
+                </div>
+                <div className="image-container">
+                    <img 
+                    src={Pic_2} 
+                    alt="برنامج تدريبي 2" 
+                    className="program-image"
+                    onMouseEnter={(e) => e.currentTarget.classList.add('hover-effect')}
+                    onMouseLeave={(e) => e.currentTarget.classList.remove('hover-effect')}
+                    />
+                    {/* <div className="image-caption">العلاج السلوكي المعرفي</div> */}
+                </div>
+                <div className="image-container">
+                    <img 
+                    src={Pic_3} 
+                    alt="برنامج تدريبي 3" 
+                    className="program-image"
+                    onMouseEnter={(e) => e.currentTarget.classList.add('hover-effect')}
+                    onMouseLeave={(e) => e.currentTarget.classList.remove('hover-effect')}
+                    />
+                    {/* <div className="image-caption">إعداد المدرب المحترف</div> */}
+                </div>
+                <div className="image-container">
+                    <img 
+                    src={Pic_4} 
+                    alt="برنامج تدريبي 4" 
+                    className="program-image"
+                    onMouseEnter={(e) => e.currentTarget.classList.add('hover-effect')}
+                    onMouseLeave={(e) => e.currentTarget.classList.remove('hover-effect')}
+                    />
+                    {/* <div className="image-caption">ممارس علم النفس الإيجابي</div> */}
+                </div>
         </div>
       </div>
     </section>
